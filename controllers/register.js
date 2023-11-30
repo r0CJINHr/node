4fefad1773d25c317020aa94c845ad8d6757ce51
= <=>
2023-11-30 15:24:30 +0300

    commitRegisr2

--- controllers/register.js
+++ controllers/register.js
@@ -1,3 +1,9 @@
+const User = require("../models/user");
+
 exports.form = (req, res) => {
   res.render("registerForm", {});
+};
+
+exports.submit = (req, res) => {
+  User.create(req.body.user, cb);
 };
